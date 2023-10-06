14577d5d92bb31cef987a5f205be8dcac20a4ed8
ranoob <whyfck@gmail.com>
2023-10-06 12:19:21 -0300

    feat: added content-type to api

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.0.105:3333',
+  baseURL: 'http://localhost:3333',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
